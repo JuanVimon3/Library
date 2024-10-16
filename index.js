a2e0120b3a0b9aa6ba733327d5e222d9788b2390
JuanVimon3 <jvillamonsalve@gmail.com>
2024-10-16 09:02:03 -0500

    create repo, add css and html

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+const library = document.getElementById('library')
+
 function Book (title, author, numPages, wellRead, info) {
   this.title = title
   this.author = author
@@ -8,10 +11,15 @@ function Book (title, author, numPages, wellRead, info) {
   }
 }
 
+function addBookToLibrary () {
+  library.innerText(myLibrary1.map(x => x))
+}
+
 const book1 = new Book('Silmarilion', 'J.R.R Tolkien', '450 pages', 'already read');
 const book2 = new Book('The Hobbit', 'J.R.R Tolkien', '350 pages', 'already read');
 const book3 = new Book('The Lord of the rings', 'J.R.R Tolkien', '1350 pages', 'already read');
 
-book1.info();
-book2.info();
-book3.info();
+const myLibrary1 = [book1, book2, book3];
+
+let myLibrary2 = [];
+
