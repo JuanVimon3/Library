648f18b3898c7c2044a860435d25baadf90a8409
JuanVimon3 <jvillamonsalve@gmail.com>
2024-10-19 11:56:45 -0500

    wip

--- index.js
+++ index.js
@@ -1,41 +1,52 @@
-document.addEventListener('DOMContentLoaded', function(){
-  const bookTable = document.getElementById('bookTable')
+document.addEventListener('DOMContentLoaded', function() {
+  const bookTable = document.getElementById('bookTable');  
 
-function Book (title, author, numPages, wellRead, info) {
-  this.title = title
-  this.author = author
-  this.numPages = numPages
-  this.wellRead = wellRead
-  this.info = () => {
-    console.log(`${this.title} by ${this.author}, ${this.numPages}, ${this.wellRead}`)
+  function Book(title, author, numPages, wellRead) {
+    this.title = title;
+    this.author = author;
+    this.numPages = numPages;
+    this.wellRead = wellRead;
   }
-}
+  const book1 = new Book('Silmarilion', 'J.R.R Tolkien', 450, true);
+  const book2 = new Book('The Hobbit', 'J.R.R Tolkien', 350, true);
+  const book3 = new Book('The Lord of the rings', 'J.R.R Tolkien', 1350, true);
 
-function addBookToLibrary () {
-  myLibrary.forEach(book => {
-    const row = document.createElement('tr')
-    row.innerHTML = 
-    `<td>${book.title}</td>
-      <td>${book.author}</td>
-      <td>${book.numPages}</td>
-      <td>${book.wellRead}</td>`;
-      bookTable.appendChild(row)
-  });
-}
+  const myLibrary1 = [book1, book2, book3];
+  function addBookToLibrary() {
+    bookTable.innerHTML = `
+    <tr>
+      <th>Title</th>
+      <th>Author</th>
+      <th>Pages</th>
+      <th>Already read</th>
+    </tr>
+    `;
+    myLibrary1.forEach(book => {
+      const row = document.createElement('tr');
+      row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.numPages}</td>
+        <td>${book.wellRead ? 'Yes' : 'No'}</td>
+      `;
+      bookTable.appendChild(row);
+    });
+  }
 
-const book1 = new Book('Silmarilion', 'J.R.R Tolkien', '450 pages', 'already read');
-const book2 = new Book('The Hobbit', 'J.R.R Tolkien', '350 pages', 'already read');
-const book3 = new Book('The Lord of the rings', 'J.R.R Tolkien', '1350 pages', 'already read');
-
-const myLibrary = [book1, book2, book3];
-
-document.querySelector('form').addEventListener('submit', enterBook())
-addBookToLibrary();
-})
-
-
-
-
+  function enterBook(event){
+    event.preventDefault()
+    const newTitle = document.getElementById('title').value;
+    const newAuthor = document.getElementById('author').value;
+    const newNumPages = document.getElementById('pages').value;
+    const newWellRead = document.getElementById('alreadyRead').checked;
+    
+    const userBook = new Book(newTitle, newAuthor, newNumPages, newWellRead)
+    myLibrary1.push(userBook)
+    addBookToLibrary();
+  }
 
 
+  addBookToLibrary();  
 
+  document.querySelector('form').addEventListener('submit', enterBook)
+});
